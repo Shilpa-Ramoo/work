b5f6835ebc7d759da703d1c5b699b4873dbefedd
Shilpa-Ramoo <sramoo@bakertilly.mu>
2025-10-03 15:29:32 +0400

    Update hexdump60.py

--- hexdump60.py
+++ hexdump60.py
@@ -1,119 +1,111 @@
-#example hex dump: 45 00 00 3c 1c 46 40 00 40 06 b1 e6 c0 a8 00 68 c0 a8 00 01
+import string
+#example hex dump 20 bytes: 45 00 00 3c 1c 46 40 00 40 06 b1 e6 c0 a8 00 68 c0 a8 00 01
 #Note:
 #byte 0 nibble 1 = version, byte 0 nibble 2 = ihl, byte 2-3 = total length, byte 9 = protocol, byte 12-15 = Source IP, byte 16 - 19 = Destination IP
 
 #all inputs are converted to string to avoid mix of data types
 #hex_arr = ['45','00','00','3c','1c','46','40','00','40','06','b1','e6','c0','a8','00','68','c0','a8','00','01']
 
-#define hex characters for comparison to check if user input hex value is valid
-hex = "0123456789abcdef"
+#stores valid hex values
+hexdigits = "0123456789abcdef"
 
+#empty variable to store non-hex values for identifying non hex values
+invalid_hex = []
 
 #a loop that will force the user to enter the IPv4 Hex dump whose length must match the entered bytes number from ip_type
 while True:
-    #an input method that asks the user to enter how many bytes are in the IPv4 Header since it can range between 20 to 60 bytes
-    #this makes the code more efficent and more flexible
-    byte_num = int(input(f"""State how many bytes are in the IPv4 Header between 20 - 60 bytes: """))
+    #an input method that asks the user to enter the IPv4 Header
+    hex_dump = str(input(f"Enter the IPv4 Header Hex Dump: "))
+    
+    #turns user input into an array of values separated by whitespace
+    hex_arr = hex_dump.split()
 
-    #checks if 
-    if byte_num >= 20 or byte_num <= 60:
-        hex_dump = str(input(f"Enter the IPv4 Header Hex Dump: "))
-        #splits the hex dump into arrays
-        hex_arr = hex_dump.split()
-        
-    #checks if the number of bytes entered matches the user input for number of bytes in IPv4 header
-    elif len(hex_arr) != byte_num:
-        print(f"Please enter an IPv4 Header with {byte_num} bytes.")
-        break
-        
-    else:
+    # string variable is used as temporary storage for storing hex_arr without whitespace
+    string = "".join(hex_dump.split())
+    
+    #turns string variable into a list with separated characters for hex validation 
+    hex_val = list(string)
+    
+    # to check the length of user input values
+    byte_num = len(hex_arr)
+
+    #checks if IPv4 header length matches the required bytes amount
+    if byte_num < 20 or byte_num > 60:
         print("Please enter bytes between 20 and 60.")
+        continue
+        
+    #if byte_num is valid,
+    elif byte_num >= 20 or byte_num <= 60:
+        # checks all string characters in hexdigit to compare with hex_val characters in a for loop
+        if all(c in hexdigits for c in hex_val) == True:
+            try:
+                #uses the value from first_char, then converts to its integer equivalent
+                #conversion done from base 16 -> hex to integer
 
+                #Provides the version of the IPv4 header
+                version = int(hex_arr[0][0], 16)
 
-    #checks if user has entered a valid hexadecimal value using defined hex set -> hex
-    for i in hex_arr:
-        if (i in hex) != True:
-            print("Hex invalid")
+                #Provides the internet header length
+                ihl = int(hex_arr[0][1],16)
 
-        else:
-            break 
+                #provides the total length of the IPv4 header
+                length = int(hex_arr[2], 16) | int(hex_arr[3], 16)
 
-   
-    if len(hex_arr) == byte_num:
-        break
+                #provides the number for protocol
+                protocol = int(hex_arr[9], 16)
+
+                #identifies which protocol is in the IPv4 header
+                match protocol:
+                    case 1:
+                        protocol_type = "ICMP"
+
+                    case 6:
+                        protocol_type = "TCP"
+
+                    case 17:
+                        protocol_type = "UDP"
+
+                    case 121:
+                        protocol_type = "SMP"
+
+                #source ip int conversions - done separately as cannot print integer with strings using concat
+                s1 = int(hex_arr[12], 16)
+                s2 = int(hex_arr[13], 16)
+                s3 = int(hex_arr[14], 16)
+                s4 = int(hex_arr[15], 16)
+
+                #same as source but for destination ip
+                d1 = int(hex_arr[16], 16)
+                d2 = int(hex_arr[17], 16)
+                d3 = int(hex_arr[18], 16)
+                d4 = int(hex_arr[19], 16)
+
+                break
+
+            except ValueError as e:
+                error = e
+                print(f"An error occurred: {error}")
+                print(f"Error type: {type(error)}")
+                continue
+                
+
+            except IndexError as e:
+                error = e
+                print(f"An error occurred: {error}")
+                print(f"Error type: {type(error)}")
+                continue
+
+            
+         # checks all string characters not in hexdigit to compare with hex_val characters in a for loop
+        elif all(c in hexdigits for c in hex_val) == False:
+            #variable invalid_hex is used to identify the non-hex values to inform user
+            invalid_hex = (all(c not in hexdigits for c in hex_val))
+            print(f"invalid hex values: {invalid_hex}")
+            print("Please enter a valid hexadecimal value.")
+            continue
         
 
-
-#uses the arrays in the hex_arr list to extract bytes
-#Extracts version byte
-byte_0 = hex_arr[0][0]
-
-#Extracts ihl byte
-byte_01 = hex_arr[0][1]
-
-#Extracts total length 1st byte
-byte_230 = hex_arr[2]
-#Extracts total length 2nd byte
-byte_231 = hex_arr[3]
-
-#Extracts protocol byte
-byte_9 = hex_arr[9]
-
-#Extracts list of source IP bytes
-byte_s1 = hex_arr[12]
-byte_s2 = hex_arr[13]
-byte_s3 = hex_arr[14]
-byte_s4 = hex_arr[15]
-
-#Extracts list of destination IP bytes
-byte_d1 = hex_arr[16]
-byte_d2 = hex_arr[17]
-byte_d3 = hex_arr[18]
-byte_d4 = hex_arr[19]
-
-
-#uses the value from first_char, then converts to its integer equivalent
-#conversion done from base 16 -> hex to integer
-
-#Provides the version of the IPv4 header
-version = int(byte_0, 16)
-
-#Provides the internet header length
-ihl = int(byte_01,16)
-
-#provides the total length of the IPv4 header
-length = int(byte_230, 16) | int(byte_231, 16)
-
-#provides the number for protocol
-protocol = int(byte_9, 16)
-
-#identifies which protocol is in the IPv4 header
-match protocol:
-    case 1:
-        protocol_type = "ICMP"
-
-    case 6:
-        protocol_type = "TCP"
-
-    case 17:
-        protocol_type = "UDP"
-
-    case 121:
-        protocol_type = "SMP"
-
-#source ip int conversions - done separately as cannot print integer with strings using concat
-s1 = int(byte_s1, 16)
-s2 = int(byte_s2, 16)
-s3 = int(byte_s3, 16)
-s4 = int(byte_s4, 16)
-
-#same as source but for destination
-d1 = int(byte_d1, 16)
-d2 = int(byte_d2, 16)
-d3 = int(byte_d3, 16)
-d4 = int(byte_d4, 16)
-
-
+                                                                                                                                                                                                                                                                                  
 #Prints the version, ihl, total length, protocol type, source & destination ip
 print(f"Version: {version}")
 print(f"IHL: {ihl}")
