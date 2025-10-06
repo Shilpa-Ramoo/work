2404b13cb0b9e370aa8c25ca99e3fd4e82cf2b38
Shilpa-Ramoo <sramoo@bakertilly.mu>
2025-10-06 10:37:32 +0400

    Update endian2.py

--- source code/endian2.py
+++ source code/endian2.py
@@ -1,1 +1,15 @@
+hex_string = str(input("Enter a 2 bytes hex values: \n"))
+hex_arr = hex_string.split()
 
+
+byte1  = int(hex_arr[0], 16)
+byte2  = int(hex_arr[1], 16)
+
+new_hex = bytes([byte1, byte2])
+ 
+big_endian = int.from_bytes(new_hex, byteorder="big") 
+little_endian = int.from_bytes(new_hex, byteorder="little") 
+
+
+print(f"Big Endian: {big_endian}")
+print(f"Little Endian: {little_endian}")
