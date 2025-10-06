768980c4a26f537426f2faf0f98b4479a256d5ea
Shilpa-Ramoo <sramoo@bakertilly.mu>
2025-10-06 10:37:53 +0400

    Update endian4.py

--- source code/endian4.py
+++ source code/endian4.py
@@ -1,1 +1,18 @@
+hex_string = str(input("Enter a 4 bytes hex values: \n"))
+hex_arr = hex_string.split()
+
+bytes_list = [0] * len(hex_arr)
+
+for i in range(len(hex_arr)):
+    bytes_list[i] = int(hex_arr[i], 16)
+
+
+new_hex = bytes(bytes_list)
+
+big_endian = int.from_bytes(new_hex, byteorder="big") 
+little_endian = int.from_bytes(new_hex, byteorder="little") 
+
+
+print(f"Big Endian: {big_endian}")
+print(f"Little Endian: {little_endian}")
 
